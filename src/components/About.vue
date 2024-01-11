0b9c18139fd10fa4609be0220f65685dbe94a29f
Rain Rahni <rain.rahni5@gmail.com>
2024-01-11 20:39:03 +0200

    add about view, W.I.P.

---
+++ src/components/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <div>
+      <img src="src/assets/coder.png">
+    </div>
+    <div>
+      <h1> About me</h1>
+      <p>
+        Hi, I'm Rain Rähni, a 2nd-year Bachelor's student in Informatics/Computer Science at Tallinn University of
+        Technology. I've honed my skills in Backend Development using Java and Spring Boot through several school
+        and personal projects. I also have experience with HTML, CSS, JavaScript, and Vue.js. I'm particularly drawn
+        to backend development with Java. I am eager to deepen my understanding and contribute to innovative solutions
+        in these fields.
+      </p>
+      <br>
+      <p>
+        As an IT student, I aspire to impact the world positively through technological developments, whether initiated by me or others. In the future, I aim to own a company
+        that helps people's lifes.Beyond my technical skills, I bring communication to my work. I'm also an active
+        football player and enjoy spending time with friends.
+      </p>
+    </div>
+  </main>
+</template>
+<style>
+main {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  color: black;
+  line-height: 2;
+}
+</style>
+<script>
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,15 +6,23 @@
         <Introduction />
       </div>
     </section>
+    <section class="about">
+      <div class="container">
+        <About />
+      </div>
+    </section>
 </template>
 <style>
 
 .intro {
   margin-left: 15%;
   padding-top: 10%;
+  padding-bottom: 10%;
 }
 </style>
 <script setup>
 
 import Introduction from "@/components/Introduction.vue";
+import About from "@/components/About.vue";
+
 </script>
