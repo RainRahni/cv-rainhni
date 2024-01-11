af24f621d4abc9d8e14d539e7354d7ba5d42a7fe
Rain Rahni <rain.rahni5@gmail.com>
2024-01-11 19:40:10 +0200

    add final logos to introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -6,9 +6,18 @@
       <h2 class="occupation"
           style="color: black;font-weight: bold">Bachelor`s informatics student</h2>
       <br>
-      <h3 class="email">rain.rahni5@gmail.com</h3>
-      <h3 class="iphone">+372 53065066</h3>
-      <h3 class="location">Tallinn, Estonia</h3>
+      <div class="info-logo">
+        <img src="src/assets/mail.png">
+        <h3 class="info">rain.rahni5@gmail.com</h3>
+      </div>
+      <div class="info-logo">
+        <img src="src/assets/phone.png">
+        <h3 class="info">+372 53065066</h3>
+      </div>
+      <div class="info-logo">
+        <img src="src/assets/location.png">
+        <h3 class="info">Tallinn, Estonia</h3>
+      </div>
       <span class="links">
         <a class="linkedIn" href="https://www.linkedin.com/in/rainrhni/" target="_blank">
           <img src="src/assets/linkedin%20logo.png">
@@ -27,6 +36,9 @@
   </main>
 </template>
 <style>
+.info {
+  margin-left: 1%;
+}
 main {
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -39,6 +51,10 @@ a {
 
 h1 {
   font-size: 55px;
+}
+.info-logo {
+  display: flex;
+  margin-top: 1%;
 }
 </style>
 <script setup>
