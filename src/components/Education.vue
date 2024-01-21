c0f8387f56b75ee8c0c67977cfc0228fbe9cf8ab
RainRahni <rain.rahni@mail.ee>
2024-01-21 10:35:46 +0200

    add school info

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -5,26 +5,49 @@
 <template>
   <div class="whole">
     <div class="education">
-      <h1>Education</h1>
-        <div class="ed-list">
-          <div class="ed-item">
+      <h1 style="text-align: center;">Education & Certificates</h1>
+        <div class="ed-item">
+          <div class="ed-info-group">
             <img style="width: 20%" src="src/assets/taltech-logo.jpg">
-            <div class="ed-info">
+            <div class="ed-info" style="margin-right: 10%">
+              <h3>Bachelor of Technology</h3>
+              <h2>Informatics/Computer Science</h2>
+              <h4>Tallinn University of Technology</h4>
             </div>
           </div>
+          <div class="ed-timeline" style="margin-right: 5%">
+            <h5>Sep-2022 to June-2025</h5>
+          </div>
         </div>
+      <div class="ed-item">
+        <div class="ed-info-group">
+          <img style="width: 20%" src="src/assets/taltech-logo.jpg">
+          <div class="ed-info" style="margin-right: 10%">
+            <h3>Bachelor of Technology</h3>
+            <h2>Informatics/Computer Science</h2>
+            <h4>Tallinn University of Technology</h4>
+          </div>
+        </div>
+        <div class="ed-timeline" style="margin-right: 5%">
+          <h5>Sep-2022 to June-2025</h5>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 
+.ed-info-group {
+  display: flex;
+  align-items: center;
+}
 .ed-item {
+  display: flex;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
-  width: 80%;
   border-radius: 5%;
-  padding: 3%;
+  align-items: center;
 }
 .whole {
   display: flex;
